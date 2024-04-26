1872e722d33411f70f6e97cce67428b578914cba
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-04-26 18:17:07 +0000

    save changes

--- js/script.js
+++ js/script.js
@@ -4,10 +4,27 @@
 // Created on: Apr 2024
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * This function displays the user age for each movie.
+ * This function uses the age f user to find out what kind of movie they can see.
  */
-function myButtonClicked() {
-  const userAge = parseInt(document.getElementById('user-Age').value)
-  
+function movieDecision() {
+  // input
+  const userAge = parseInt(document.getElementById('user-age').value)
+
+  // process
+  if (userAge >= 17) {
+    // output
+    document.getElementById('movie-decision').innerHTML = 'You can go see an R rated movie alone.'
+  } else if (userAge >= 13) {
+    // output
+    document.getElementById('movie-decision').innerHTML = 'You can go see a PG-13 rated movie alone.'
+  } else if (userAge >= 5) {
+    // output
+    document.getElementById('movie-decision').innerHTML = 'You can go see a G or PG rated movie alone.'
+  } else {
+    // output
+    document.getElementById('movie-decision').innerHTML = "Uh, You're too young for most things."
+  }
 }
